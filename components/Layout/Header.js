a3c275f3debc5451496b117f6f477412548964e4
parag <paragjain528@gmail.com>
2022-11-23 22:42:01 +0530

    Header component added

---
+++ components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+import mealsImg from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>React meals</h1>
+      <HeaderCartButton></HeaderCartButton>
+      </header>
+
+      <div className={classes['main-image']}>
+        <img src={mealsImg} alt="" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
---
+++ components/Layout/HeaderCartButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = () => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon></CartIcon>
+      </span>
+      <span>your cart</span>
+      <span className={classes.badge}>4</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
