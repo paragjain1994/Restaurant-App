bfd8f8a3dc9a26756859945904dd11325d73f67c
parag <paragjain528@gmail.com>
2023-01-02 12:19:24 +0530

    merging same item in the cart

--- store/CartProvider.js
+++ store/CartProvider.js
@@ -1,30 +1,40 @@
-import React , {useState} from 'react';
-import CartContext from './cart-context';
+import React, { useState } from "react";
+import CartContext from "./cart-context";
 
 const CartProvider = (props) => {
- const [items,updateItems] = useState([]);
+  const [items, updateItems] = useState([]);
 
- const addItemToCartHandler=(item)=>{
-  console.log(item.quantity);
-  updateItems([...items,item]);
-  console.log('inside addItemToCartHandler',cartContext);
- }
+  const addItemToCartHandler = (item) => {
+    console.log(item.quantity); // quantity and id of the item which we have added from MealItemForm.js comp
+    console.log(item.id);
 
- 
- const removeItemFromCartHandler=(id)=>{}
+    const idx = items.findIndex((i) => i.id === item.id);
 
+    if (idx === -1) {
+      updateItems([...items, item]);
+    } else {
+      let temp = [...items];
+      temp[idx].quantity = Number(temp[idx].quantity) + Number(item.quantity);
+      updateItems(temp);
+    }
 
+    console.log("inside addItemToCartHandler", cartContext);
+  };
+
+  const removeItemFromCartHandler = () => {};
+
+  console.log("open " + items.length);
 
   const cartContext = {
     items: items,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
-    msg: 'I am accessible anywhere'
+    msg: "I am accessible anywhere",
   };
-
+  console.log("carttt " + cartContext.items);
   return (
     <CartContext.Provider value={cartContext}>
-      {console.log('Inside CartContext.Provider', cartContext)}
+      {console.log("Inside CartContext.Provider", cartContext)}
       {props.children}
     </CartContext.Provider>
   );
